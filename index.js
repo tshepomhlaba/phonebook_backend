79cc126cfc3129b74c27056d37ba71490327eb03
Tshepo <mhlabatshepo@gmail.com>
2025-08-21 19:27:39 +0200

    feat: exercise 3.2

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  response.send(
+    `<div>Phonebook has info for ${
+      persons.length
+    } people <br> <br> ${new Date()}  </div>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
