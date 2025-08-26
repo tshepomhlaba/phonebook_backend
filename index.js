3226345f91a5f79d8efa7ff5e3e36a89ad80ad0c
Tshepo <mhlabatshepo@gmail.com>
2025-08-26 14:43:23 +0200

    feat: exercise 3.9

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require('cors')
+
+app.use(cors())
 
 //Persons Array with phoneboook numbers
 let persons = [
