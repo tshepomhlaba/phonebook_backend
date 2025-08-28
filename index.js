f3b0193385003f7ec335fe02c767f131069da055
Tshepo <mhlabatshepo@gmail.com>
2025-08-28 15:02:57 +0200

    feat: change port functionality && add middleware for dist file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const cors = require('cors')
-
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
+app.use(express.static('dist'))
 
 //Persons Array with phoneboook numbers
 let persons = [
@@ -103,7 +103,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
