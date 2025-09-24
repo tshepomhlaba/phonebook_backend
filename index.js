c273171feb7fdcef784dc195a5412dfa90786d2b
Tshepo <mhlabatshepo@gmail.com>
2025-09-24 16:38:41 +0200

    feat: exercise 3.13 && 3.14

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const app = express();
 const cors = require("cors");
-app.use(cors());
-app.use(express.static('dist'))
+const Person = require("./models/person");
+const app = express();
 
-//Persons Array with phoneboook numbers
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+app.use(cors());
+app.use(express.static("dist"));
+
+let persons = [];
 
 morgan.token("body", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
@@ -37,38 +18,34 @@ app.use(morgan(":method :url :status :response-time ms :body"));
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send("<p>Hello World</p>");
-});
+// app.get("/", (request, response) => {
+//   response.send("<p>Hello World</p>");
+// });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
-app.get("/info", (request, response) => {
-  response.send(
-    `<div>Phonebook has info for ${
-      persons.length
-    } people <br> <br> ${new Date()}  </div>`
-  );
-});
+// app.get("/info", (request, response) => {
+//   response.send(
+//     `<div>Phonebook has info for ${
+//       persons.length
+//     } people <br> <br> ${new Date()}  </div>`
+//   );
+// });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+// app.get("/api/persons/:id", (request, response) => {
+//   const id = request.params.id;
+//   const person = persons.find((person) => person.id === id);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
-
-const generateId = () => {
-  const maxId =
-    persons.length > 0 ? Math.max(...persons.map((p) => Number(p.id))) : 0;
-  return String(maxId + 1);
-};
+//   if (person) {
+//     response.json(person);
+//   } else {
+//     response.status(404).end();
+//   }
+// });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -79,29 +56,28 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  // if (persons.find((person) => person.name === body.name)) {
+  //   return response.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const persons = persons.filter((person) => person.id !== id);
+// app.delete("/api/persons/:id", (request, response) => {
+//   const id = request.params.id;
+//   const persons = persons.filter((person) => person.id !== id);
 
-  response.status(204).end();
-});
+//   response.status(204).end();
+// });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -1,56 +1,56 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const password = process.argv[2];
+// const password = process.argv[2];
 
-const url = `mongodb+srv://tshepo:${password}@cluster0.ykxlxpx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const url = ``;
 
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
-mongoose.connect(url);
+// mongoose.connect(url);
 
-const PersonSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
+// const PersonSchema = new mongoose.Schema({
+//   name: String,
+//   number: String,
+// });
 
-const Person = mongoose.model("Person", PersonSchema);
+// const Person = mongoose.model("Person", PersonSchema);
 
-if(password) {
-if(process.argv.length === 3) {
-    Person.find({}).then((result) => {
-    result.forEach((person) => {
-      console.log(person);
-    });
-    mongoose.connection.close();
-  }).catch((error) => {
-    console.log("Error retrieving person:", error);
-    mongoose.connection.close();
-  });
-} else if (process.argv.length === 5) {
-const person = new Person({
-  name: process.argv[3],
-  number: process.argv[4],
-});
+// if(password) {
+// if(process.argv.length === 3) {
+//     Person.find({}).then((result) => {
+//     result.forEach((person) => {
+//       console.log(person);
+//     });
+//     mongoose.connection.close();
+//   }).catch((error) => {
+//     console.log("Error retrieving person:", error);
+//     mongoose.connection.close();
+//   });
+// } else if (process.argv.length === 5) {
+// const person = new Person({
+//   name: process.argv[3],
+//   number: process.argv[4],
+// });
 
-    person
-  .save()
-  .then((result) => {
-    console.log(`Added ${person.name} ${person.number} to phonebook`);
-    mongoose.connection.close();
-  })
-  .catch((error) => {
-    console.log("Error saving person:", error);
-    mongoose.connection.close();
-  });
-} else {
-  console.log("Usage: node mongo.js <password> <name> <number>, to create a new user entry")
-  console.log("Usage: node mongo.js <password>, to get all the user entries" )
-  process.exit(1);  
-}
-} else {
-console.log("Error: Please provide password")
-  process.exit(1);  
-}
+//     person
+//   .save()
+//   .then((result) => {
+//     console.log(`Added ${person.name} ${person.number} to phonebook`);
+//     mongoose.connection.close();
+//   })
+//   .catch((error) => {
+//     console.log("Error saving person:", error);
+//     mongoose.connection.close();
+//   });
+// } else {
+//   console.log("Usage: node mongo.js <password> <name> <number>, to create a new user entry")
+//   console.log("Usage: node mongo.js <password>, to get all the user entries" )
+//   process.exit(1);  
+// }
+// } else {
+// console.log("Error: Please provide password")
+//   process.exit(1);  
+// }
 
 
 
